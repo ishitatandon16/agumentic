1be229922e0b2050ff14eec1de9eacc4cbe278f4
ishitatandon16 <ishita.tandon_cs20@gla.ac.in>
2023-11-01 00:04:38 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Banner from './components/Banner';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      
+    <Banner></Banner>
+    <Footer></Footer>
+    
+    </>
+  );
+}
+
+export default App;
